2577f3282055330b35ced64fe4bac5e2f6f25a3a
pierre-lamarche <lamarche.p@gmail.com>
2016-08-26 13:45:45 +0200

    Create RUN.R

---
+++ RUN.R
@@ -0,0 +1,23 @@
+packages.available = installed.packages()
+if (!"tcltk" %in% row.names(packages.available)) install.packages("tcltk")
+
+library(tcltk)
+
+
+#### dl the data
+url_map = "https://wxs-telechargement.ign.fr/oikr5jryiph0iwhw36053ptm/telechargement/inspire/GEOFLA_THEME-COMMUNE_2016$GEOFLA_2-2_COMMUNE_SHP_LAMB93_FXX_2016-06-28/file/GEOFLA_2-2_COMMUNE_SHP_LAMB93_FXX_2016-06-28.7z"
+url_filosofi = "http://www.insee.fr/fr/ppp/bases-de-donnees/donnees-detaillees/filosofi/filosofi-2012/indic-struct-distrib-revenu/indic-struct-distrib-revenu-communes-2012.zip"
+
+folder = tk_choose.dir(default=getwd(),"Choose the folder where to store the data:")
+
+dir.create(paste0(folder,"/map"))
+download.file(url=url_map,destfile = paste0(folder,"/map/archive.7z"),mode="wb")
+unz(paste0(folder,"/map/archive.7z"),"COMMUNE.SHP",open="r")
+
+dir.create(paste0(folder,"/data"))
+download.file(url=url_filosofi,destfile = paste0(folder,"/data/archive.zip"),mode="wb")
+unzip(zipfile=paste0(folder,"/data/archive.zip"),exdir=paste0(folder,"/data"))
+
+
+
+
